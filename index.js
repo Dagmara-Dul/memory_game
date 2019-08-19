60de0368c838049c90c8559f553877a12802e8ae
Dag4 <dul.dagmara@gmail.com>
2019-08-19 09:09:18 +0200

    memory game start

---
+++ index.js
@@ -0,0 +1,11 @@
+$(function(){
+
+var cards = ["1.png","2.png","4.png","10.png","8.png", "1.png","2.png","5.png","7.png","11.png","3.png","6.png","3.png","4.png", "5.png", "6.png", "7.png", "8.png", "10.png", "11.png"];
+
+console.log(cards);
+
+function cardUp(number){
+    console.log("number");
+}
+
+});
